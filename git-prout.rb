9c5737dd65eb82582d4c033254856f84ac73a306
tsuyoshiwada <v23.b.wada@gmail.com>
2017-04-02 23:57:40 +0900

    Fix formula

--- git-prout.rb
+++ git-prout.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-class Gitprout < Formula
+class GitProut < Formula
   homepage "https://github.com/tsuyoshiwada/git-prout"
   url "https://github.com/tsuyoshiwada/git-prout/releases/download/0.0.1/darwin_amd64_git-prout"
   sha256 "7d923836269aca5eea16ea95ee9d462f8f77d8d2e0ee492d6927dff5091f62c0"
@@ -8,6 +8,7 @@ class Gitprout < Formula
   version "0.0.1"
 
   def install
+    system "mv", "darwin_amd64_git-prout", "git-prout"
     bin.install "git-prout"
   end
 end
